88dbd01d033f65e0a20af10ed6b10c11b72d296c
faizan <117802825+FaizanHussainhsp@users.noreply.github.com>
2024-03-27 20:12:38 +0500

    email verfication is resolved

--- src/Screens/SignUpScreen/BusinessLogic.js
+++ src/Screens/SignUpScreen/BusinessLogic.js
@@ -1,69 +1,95 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { collection, doc, setDoc } from 'firebase/firestore';
-import { useState } from 'react';
-import { Alert, ToastAndroid } from 'react-native';
-import { FIREBASE_AUTH, FIREBASE_APP, FIREBASE_DB } from "../../../firebaseConfig";
+import { collection, doc, setDoc } from "firebase/firestore";
+import { useState } from "react";
+import { Alert, ToastAndroid } from "react-native";
+import {
+  FIREBASE_AUTH,
+  FIREBASE_APP,
+  FIREBASE_DB
+} from "../../../firebaseConfig";
+import { firebase } from "@react-native-firebase/firestore";
 
-const SignUpBusinessLogic = ({children, navigation}) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [load, setLoad] = useState(false);
-    const [isEmailValid, setIsEmailValid] = useState(true)
-    const [isPasswordValid, setIsPasswordValid] = useState(true)
-    const [validationError, setValidationError] = useState(false)
+const SignUpBusinessLogic = ({ children, navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [load, setLoad] = useState(false);
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const [validationError, setValidationError] = useState(false);
 
-    const validateEmail = (email, type) => {
-        setEmail(email)
-        // Regular expression for email validation
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        // Test the email against the regex
-        console.log('email valid', emailRegex.test(email))
-        setIsEmailValid(emailRegex.test(email))
-    }
-    const validatePassword = (password) => {
-        setPassword(password)
-        // Regular expressions for password validation
-        const upperCaseRegex = /[A-Z]/;
-        const lowerCaseRegex = /[a-z]/;
-        const numberRegex = /[0-9]/;
-        const symbolRegex = /[$&+,:;=?@#|'<>.^*()%!-]/;
-        const minLength = 8;
-        // Test the password against the regex
-        const isUpperCaseValid = upperCaseRegex.test(password);
-        const isLowerCaseValid = lowerCaseRegex.test(password);
-        const isNumberValid = numberRegex.test(password);
-        const isSymbolValid = symbolRegex.test(password);
-        const isLengthValid = password.length >= minLength;
-        const isPasswordValid = isUpperCaseValid && isLowerCaseValid && isNumberValid && isSymbolValid && isLengthValid;
-        setIsPasswordValid(isPasswordValid)
-    };
-    const handleRegister = async () => {
-        setValidationError(true)
-        if (email.length > 0 && password.length > 0 && confirmPassword.length > 0) {
-        if(isEmailValid){
-        const auth = FIREBASE_AUTH
+  const validateEmail = (email, type) => {
+    setEmail(email);
+    // Regular expression for email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // Test the email against the regex
+    console.log("email valid", emailRegex.test(email));
+    setIsEmailValid(emailRegex.test(email));
+  };
+  const validatePassword = (password) => {
+    setPassword(password);
+    // Regular expressions for password validation
+    const upperCaseRegex = /[A-Z]/;
+    const lowerCaseRegex = /[a-z]/;
+    const numberRegex = /[0-9]/;
+    const symbolRegex = /[$&+,:;=?@#|'<>.^*()%!-]/;
+    const minLength = 8;
+    // Test the password against the regex
+    const isUpperCaseValid = upperCaseRegex.test(password);
+    const isLowerCaseValid = lowerCaseRegex.test(password);
+    const isNumberValid = numberRegex.test(password);
+    const isSymbolValid = symbolRegex.test(password);
+    const isLengthValid = password.length >= minLength;
+    const isPasswordValid =
+      isUpperCaseValid &&
+      isLowerCaseValid &&
+      isNumberValid &&
+      isSymbolValid &&
+      isLengthValid;
+    setIsPasswordValid(isPasswordValid);
+  };
+  const handleRegister = async () => {
+    setValidationError(true);
+    if (email.length > 0 && password.length > 0 && confirmPassword.length > 0) {
+      if (isEmailValid) {
+        const auth = FIREBASE_AUTH;
         setLoad(true);
         if (password !== confirmPassword) {
-          Alert.alert("Password Mismatch", "Please enter the same password in both fields");
+          Alert.alert(
+            "Password Mismatch",
+            "Please enter the same password in both fields"
+          );
           setLoad(false);
           return;
         }
         try {
-          const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+          const userCredential = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+          );
           const user = userCredential.user;
-          const userRef = collection(FIREBASE_DB, "users");
-          // Create a document reference using the user's UID
-          const userDoc = doc(userRef, user.uid);
-          // Data to be stored in the Firestore document
-          const userData = {
-            email: email,
-            user_id:user.uid
-          };
-          // Set data in Firestore document
-          await setDoc(userDoc, userData);
-          // Navigate to login screen after successful registration
-          navigation.navigate('login');
+          const userEmail = firebase.auth().currentUser;
+          userEmail
+            .sendEmailVerification()
+            .then(async () => {
+              console.log("Verification email sent successfully!");
+              const userRef = collection(FIREBASE_DB, "users");
+              // Create a document reference using the user's UID
+              const userDoc = doc(userRef, user.uid);
+              // Data to be stored in the Firestore document
+              const userData = {
+                email: email,
+                user_id: user.uid
+              };
+              // Set data in Firestore document
+              await setDoc(userDoc, userData);
+              // Navigate to login screen after successful registration
+              navigation.navigate("login");
+            })
+            .catch((error) => {
+              console.error("Error sending verification email:", error);
+            });
         } catch (error) {
           console.error("Registration Failed:", error);
           Alert.alert("Registration Failed", error.message);
@@ -71,35 +97,36 @@ const SignUpBusinessLogic = ({children, navigation}) => {
           // Reset loading state regardless of success or failure
           setLoad(false);
         }
-      }else {
-        ToastAndroid.show('Invalid Email', ToastAndroid.SHORT);
+      } else {
+        ToastAndroid.show("Invalid Email", ToastAndroid.SHORT);
       }
-      }else{
-        ToastAndroid.show('Empty Email or Password', ToastAndroid.SHORT);
-    }}  
-    const goBack = ()=>{
-        navigation.navigate('login');
+    } else {
+      ToastAndroid.show("Empty Email or Password", ToastAndroid.SHORT);
     }
-    return children({
-        navigation,
-        email,
-        setEmail,
-        password,
-        setPassword,
-        confirmPassword,
-        setConfirmPassword, 
-        load,
-        setLoad,
-        isEmailValid, 
-        setIsEmailValid,
-        isPasswordValid, 
-        setIsPasswordValid, 
-        validationError,
-        setValidationError,
-        validateEmail,
-        validatePassword,
-        handleRegister,
-        goBack
-    });
-}
+  };
+  const goBack = () => {
+    navigation.navigate("login");
+  };
+  return children({
+    navigation,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    confirmPassword,
+    setConfirmPassword,
+    load,
+    setLoad,
+    isEmailValid,
+    setIsEmailValid,
+    isPasswordValid,
+    setIsPasswordValid,
+    validationError,
+    setValidationError,
+    validateEmail,
+    validatePassword,
+    handleRegister,
+    goBack
+  });
+};
 export default SignUpBusinessLogic;
--- src/Screens/SignupScreen.js
+++ src/Screens/SignupScreen.js
@@ -1,10 +1,21 @@
-import { FontAwesome5 } from '@expo/vector-icons';
-import { StatusBar } from 'expo-status-bar';
+import { FontAwesome5 } from "@expo/vector-icons";
+import { StatusBar } from "expo-status-bar";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import React, { useState } from 'react';
-import { ActivityIndicator, Alert, StyleSheet, Text, TextInput, ToastAndroid, TouchableOpacity, View } from 'react-native';
-import { FIREBASE_AUTH, FIREBASE_DB } from '../../firebaseConfig';
-import { collection, doc, setDoc } from 'firebase/firestore';
+import React, { useState } from "react";
+import {
+  ActivityIndicator,
+  Alert,
+  StyleSheet,
+  Text,
+  TextInput,
+  ToastAndroid,
+  TouchableOpacity,
+  View
+} from "react-native";
+import { FIREBASE_AUTH, FIREBASE_DB } from "../../firebaseConfig";
+import { collection, doc, setDoc } from "firebase/firestore";
+import { firebase } from "@react-native-firebase/auth";
+import * as firebaseAuth from "firebase/auth";
 
 const UserIcon = () => {
   return (
@@ -12,31 +23,28 @@ const UserIcon = () => {
       <FontAwesome5 name="user" style={styles.userIcon} />
     </View>
   );
-}
+};
 
-export default function SignupScreen({navigation}) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+export default function SignupScreen({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [load, setLoad] = useState(false);
-  const [isEmailValid, setIsEmailValid] = useState(true)
-  const [isPasswordValid, setIsPasswordValid] = useState(true)
-  const [validationError, setValidationError] = useState(false)
-
-
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const [validationError, setValidationError] = useState(false);
   const validateEmail = (email, type) => {
-    setEmail(email)
+    setEmail(email);
     // Regular expression for email validation
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     // Test the email against the regex
-    console.log('email valid', emailRegex.test(email))
-    setIsEmailValid(emailRegex.test(email))
+    console.log("email valid", emailRegex.test(email));
+    setIsEmailValid(emailRegex.test(email));
     return emailRegex.test(email);
-  }
-
+  };
 
   const validatePassword = (password) => {
-    setPassword(password)
+    setPassword(password);
     // Regular expressions for password validation
     const upperCaseRegex = /[A-Z]/;
     const lowerCaseRegex = /[a-z]/;
@@ -50,57 +58,73 @@ export default function SignupScreen({navigation}) {
     const isNumberValid = numberRegex.test(password);
     const isSymbolValid = symbolRegex.test(password);
     const isLengthValid = password.length >= minLength;
-    const isPasswordValid = isUpperCaseValid && isLowerCaseValid && isNumberValid && isSymbolValid && isLengthValid;
-    setIsPasswordValid(isPasswordValid)
-};
-
-
+    const isPasswordValid =
+      isUpperCaseValid &&
+      isLowerCaseValid &&
+      isNumberValid &&
+      isSymbolValid &&
+      isLengthValid;
+    setIsPasswordValid(isPasswordValid);
+  };
 
   const handleRegister = async () => {
-    setValidationError(true)
+    console.log("okay");
+    setValidationError(true);
     if (email.length > 0 && password.length > 0 && confirmPassword.length > 0) {
-    if(isEmailValid){
-    const auth = FIREBASE_AUTH
-    setLoad(true);
-    if (password !== confirmPassword) {
-      Alert.alert("Password Mismatch", "Please enter the same password in both fields");
-      setLoad(false);
-      return;
-    }
-    try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      const user = userCredential.user;
-      const userRef = collection(FIREBASE_DB, "users");
-      // Create a document reference using the user's UID
-      const userDoc = doc(userRef, user.uid);
-      // Data to be stored in the Firestore document
-      const userData = {
-        email: email,
-        user_id:user.uid
-      };
-      // Set data in Firestore document
-      console.log("userData is", userData)
-      await setDoc(userDoc, userData);
-      // Navigate to login screen after successful registration
-      navigation.navigate('login');
-    } catch (error) {
-      console.error("Registration Failed:", error);
-      Alert.alert("Registration Failed", error.message);
-    } finally {
-      // Reset loading state regardless of success or failure
-      setLoad(false);
-    }
-  }else {
-    ToastAndroid.show('Invalid Email', ToastAndroid.SHORT);
-  }
-  }else{
-    ToastAndroid.show('Empty Email or Password', ToastAndroid.SHORT);
-  }}
-  
+      if (isEmailValid) {
+        const auth = FIREBASE_AUTH;
+        setLoad(true);
+        if (password !== confirmPassword) {
+          Alert.alert(
+            "Password Mismatch",
+            "Please enter the same password in both fields"
+          );
+          setLoad(false);
+          return;
+        }
+        try {
+          const userCredential = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+          );
 
-  const goBack = ()=>{
-    navigation.navigate('login');
-}
+          const user = userCredential.user;
+          // await firebase.auth().currentUser.sendEmailVerification();
+          await firebaseAuth.sendEmailVerification();
+          Alert.alert("please check your email");
+
+          const userRef = collection(FIREBASE_DB, "users");
+          // Create a document reference using the user's UID
+          const userDoc = doc(userRef, user.uid);
+          // Data to be stored in the Firestore document
+          const userData = {
+            email: email,
+            user_id: user.uid
+          };
+
+          // Set data in Firestore document
+          await setDoc(userDoc, userData);
+          // Navigate to login screen after successful registration
+          navigation.navigate("login");
+        } catch (error) {
+          console.error("Registration Failed:", error);
+          Alert.alert("Registration Failed", error.message);
+        } finally {
+          // Reset loading state regardless of success or failure
+          setLoad(false);
+        }
+      } else {
+        ToastAndroid.show("Invalid Email", ToastAndroid.SHORT);
+      }
+    } else {
+      ToastAndroid.show("Empty Email or Password", ToastAndroid.SHORT);
+    }
+  };
+
+  const goBack = () => {
+    navigation.navigate("login");
+  };
 
   return (
     <View style={styles.container}>
@@ -109,32 +133,51 @@ export default function SignupScreen({navigation}) {
         <Text style={styles.title}>Create an Account</Text>
 
         <View style={styles.inputContainer}>
-        <FontAwesome5 name="envelope" style={styles.icon} />
+          <FontAwesome5 name="envelope" style={styles.icon} />
           <TextInput
             style={styles.input}
             placeholder="Email Address"
             value={email}
             onChangeText={(text) => validateEmail(text)}
           />
-         
-         
         </View>
-        {!isEmailValid && validationError &&    <Text style={{color:'red', alignSelf:'flex-end', marginBottom:10 , width:'50%'}}>Please Enter a valid email</Text>}
+        {!isEmailValid && validationError && (
+          <Text
+            style={{
+              color: "red",
+              alignSelf: "flex-end",
+              marginBottom: 10,
+              width: "50%"
+            }}
+          >
+            Please Enter a valid email
+          </Text>
+        )}
 
         <View style={styles.inputContainer}>
-        <FontAwesome5 name="lock" style={styles.icon} />
+          <FontAwesome5 name="lock" style={styles.icon} />
           <TextInput
             style={styles.input}
             placeholder="Password"
             secureTextEntry={true}
             value={password}
             onChangeText={(text) => validatePassword(text)}
-          />    
+          />
         </View>
-        {!isPasswordValid && validationError &&    
-           <Text style={{color:'red', alignSelf:'flex-end', marginBottom:10, width:'80%'}}>Password needs 8+ characters, a number, symbol, and uppercase letter</Text>}
+        {!isPasswordValid && validationError && (
+          <Text
+            style={{
+              color: "red",
+              alignSelf: "flex-end",
+              marginBottom: 10,
+              width: "80%"
+            }}
+          >
+            Password needs 8+ characters, a number, symbol, and uppercase letter
+          </Text>
+        )}
         <View style={styles.inputContainer}>
-        <FontAwesome5 name="lock" style={styles.icon} />
+          <FontAwesome5 name="lock" style={styles.icon} />
           <TextInput
             style={styles.input}
             placeholder="Confirm Password"
@@ -142,14 +185,24 @@ export default function SignupScreen({navigation}) {
             value={confirmPassword}
             onChangeText={(text) => setConfirmPassword(text)}
           />
-          
         </View>
-        {password != confirmPassword && validationError &&    
-           <Text style={{color:'red', alignSelf:'flex-end', marginBottom:10, }}>Passwords do not match</Text>}
+        {password != confirmPassword && validationError && (
+          <Text
+            style={{ color: "red", alignSelf: "flex-end", marginBottom: 10 }}
+          >
+            Passwords do not match
+          </Text>
+        )}
 
-        <TouchableOpacity style={styles.registerButton} onPress={handleRegister}>
-          {load ? <ActivityIndicator size="small" color="white" /> :
-            <Text style={styles.buttonText}>Register</Text>}
+        <TouchableOpacity
+          style={styles.registerButton}
+          onPress={handleRegister}
+        >
+          {load ? (
+            <ActivityIndicator size="small" color="white" />
+          ) : (
+            <Text style={styles.buttonText}>Register</Text>
+          )}
         </TouchableOpacity>
 
         <View style={styles.google}>
@@ -159,7 +212,9 @@ export default function SignupScreen({navigation}) {
         </View>
 
         <TouchableOpacity style={styles.back} onPress={goBack}>
-          <Text style={{ fontSize: 15, color: 'blue' }}>Already have an account? Sign in</Text>
+          <Text style={{ fontSize: 15, color: "blue" }}>
+            Already have an account? Sign in
+          </Text>
         </TouchableOpacity>
       </View>
 
@@ -171,41 +226,42 @@ export default function SignupScreen({navigation}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5F5F5',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F5F5F5",
+    alignItems: "center",
+    justifyContent: "center"
   },
   userIconContainer: {
-    backgroundColor: '#5fa9c7',
+    backgroundColor: "#5fa9c7",
     width: 100,
     height: 100,
     borderRadius: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 15, marginTop:'10%',
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 15,
+    marginTop: "10%"
   },
   userIcon: {
     fontSize: 60,
-    color: 'white',
+    color: "white"
   },
   formContainer: {
-    width: '90%',
-    backgroundColor: 'white',
+    width: "90%",
+    backgroundColor: "white",
     borderRadius: 15,
     padding: 20,
     marginTop: 20,
-    alignItems: 'center',
+    alignItems: "center"
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
+    fontWeight: "bold",
+    marginBottom: 20
   },
   input: {
     flex: 1,
     height: 40,
     paddingLeft: 10,
-    color: 'black',
+    color: "black"
   },
   // inputContainer: {
   //   flexDirection: 'row',
@@ -215,42 +271,44 @@ const styles = StyleSheet.create({
   //   marginBottom: 20,
   // },
   inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between', // Align items evenly in the container
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between", // Align items evenly in the container
     borderBottomWidth: 1,
-    borderBottomColor: 'lightgray',
+    borderBottomColor: "lightgray",
     marginBottom: 20,
-    position: 'relative', // Ensure the container is positioned relatively for absolute positioning of icons
+    position: "relative" // Ensure the container is positioned relatively for absolute positioning of icons
   },
   registerButton: {
-    backgroundColor: 'blue',
+    backgroundColor: "blue",
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderRadius: 5,
-    marginTop: 20, width:110,
+    marginTop: 20,
+    width: 110
   },
   buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
   },
   back: {
-    marginTop: 15,
+    marginTop: 15
   },
   icon: {
     fontSize: 20,
     marginRight: 10,
-    color: 'gray',
+    color: "gray"
   },
   google: {
-    width: '70%',
+    width: "70%",
     height: 45,
     borderRadius: 30,
-    backgroundColor: 'blue',
-    marginTop: 30, marginBottom:20,
+    backgroundColor: "blue",
+    marginTop: 30,
+    marginBottom: 20,
     padding: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
